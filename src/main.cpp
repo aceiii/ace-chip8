9a2379c8934b44f6b549f766c681d0a5d564cd34
Borin Ouch <borin.ouch@gmail.com>
2024-03-26 23:49:35 -0700

    Add raylib, create and draw window to make sure it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <spdlog/spdlog.h>
+#include <raylib.h>
 
 auto main() -> int {
     spdlog::info("Hello, world!!!");
+
+    int width = 800;
+    int height = 600;
+
+    InitWindow(width, height, "CHIP-8");
+    SetExitKey(KEY_ESCAPE);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
